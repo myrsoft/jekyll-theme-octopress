0ae198b1abb6a3e38108f7806bb0056a22d76ea3
Raven Du <Raven.Du@hotmail.com>
2018-11-19 16:22:00 +0800

    init project

---
+++ about.md
@@ -0,0 +1,21 @@
+---
+layout: page
+title: About
+permalink: /about/
+---
+
+This Jekyll theme is the jekyll-theme-octopress .
+
+You can find the source code for jekyll-theme-octopress at GitHub:
+[MyrSoft Studio][jekyll-organization] /
+[jekyll-theme-octopress][jekyll-theme-octopress]
+
+You can find the source code for Jekyll at GitHub:
+[jekyll][jekyll-organization] /
+[jekyll](https://github.com/jekyll/jekyll)
+
+
+[myrsoft-origanization]: https://github.com/myrsoft
+[jekyll-theme-octopress]: https://github.com/myrsoft/jekyll-theme-octopress
+
+[jekyll-organization]: https://github.com/jekyll
---
+++ config.rb
@@ -0,0 +1,35 @@
+require 'sass-globbing'
+# Require any additional compass plugins here.
+
+project_type = :stand_alone
+
+# Publishing paths
+# Set this to the root of your project when deployed:
+http_path = "/jekyll-theme-octopress"
+css_dir = "/assets/stylesheets"
+http_images_path = "/jekyll-theme-octopress/assets/images"
+http_generated_images_path = "/jekyll-theme-octopress/assets/images"
+http_fonts_path = "/jekyll-theme-octopress/assets/fonts"
+
+# Local development paths
+sass_dir = "_sass"
+images_dir = "/assets/images"
+fonts_dir = "/assets/fonts"
+javascripts_dir = "/assets/javascripts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+line_comments = false
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
